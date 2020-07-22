3496dee7848f952c9915673683bff7db3fe6546d
ly92 <1364757394@qq.com>
2020-07-22 16:39:49 +0800

    first

---
+++ TANCMS/spiders/article.py
@@ -0,0 +1,38 @@
+import scrapy
+import re
+
+from TANCMS.items import GzhItem
+
+class ArticleSpider(scrapy.Spider):
+    name = "article"
+    allowed_domains = ["mp.weixin.qq.com"]
+#     start_urls = [
+#         'https://mp.weixin.qq.com/s?src=11&timestamp=1595380912&ver=2475&signature=FQ19As8jwwNNdmOBZPtCFGUvNzAszaTve9c7oCiYmE5xTzAIzNrK1HiQxcPq57av2qG1hvtgbF6WZrgmI4E0RN0SSMaIi*ZlE5Wadot5LjqHr2t3E0sbvB95gn4FMSwG&new=1',
+# 'https://mp.weixin.qq.com/s?src=11&timestamp=1595380912&ver=2475&signature=mFNO9u3V8pK02jb4axnpc9Q8qM5hqxnLkg*xIZla-nLACMbMLMR6XpnCwGdj7TfPPtXh1RBdfickm0y*q-38CMfQcdw-f9*gwcB3XqJuZk5RKUmjy0IVHz4N8CjGZ3yy&new=1',
+# 'https://mp.weixin.qq.com/s?src=11&timestamp=1595380912&ver=2475&signature=iZdx1JMTZ0W35QknV*QvxvSLvfJBo*xHop708anUeyaMJXI0lxIzCb9g-sHMb5apNjkr7glXakUP-uIHeO6Nff1NSlJkcktyhFeLvqdCSUsst5eLMsMfFyLwmP7JUuhU&new=1',
+# 'https://mp.weixin.qq.com/s?src=11&timestamp=1595380912&ver=2475&signature=0urGvIQ5CEbFbjmgXLeeGNBwK0yom*t2kv6oykFdhn8L0IFGWuChZ1vVRXIGh3kpuKRQjNahJLg6k2FIoInazTsqI5CGvhWzoOxVpuZwzdyMzzEzXGA6uK9jabSd*EUp&new=1',
+# 'https://mp.weixin.qq.com/s?src=11&timestamp=1595380912&ver=2475&signature=0urGvIQ5CEbFbjmgXLeeGNBwK0yom*t2kv6oykFdhn*z2uNXGiz2XOn87qdyDUkRijTK6ikS5JQRBhnQwzcOY8d95daQF3gBVtb82e8ZJJYRRuUK-HYtCDYTau-qtUjU&new=1',
+# 'https://mp.weixin.qq.com/s?src=11&timestamp=1595380912&ver=2475&signature=4giUnjPieXIDFQLEVqsJTkMdftSYsRl02SrYvmaKWstu5Qy6PjmusXDttrVBpPtRGRQWb*JvrkMb*2lrQu0vnKhlfZWVh9l64YnoIZPM684bOEdGf3CQc8doHq3cLvO8&new=1',
+# 'https://mp.weixin.qq.com/s?src=11&timestamp=1595380912&ver=2475&signature=ZamHFf3WBKmYRVDfeY-pYspZGARoFFwcY3MCb4c5QjjHJGucNScNhBXKBo-H0J5*znWdicw-Fi9o3cGJQZNzcTHK3y3FVhZ9puSnkLFkmLFH19VUErPhP50UQhp99t7o&new=1',
+# 'https://mp.weixin.qq.com/s?src=11&timestamp=1595380912&ver=2475&signature=2J2uGMzgaDdfZhLrmD2Yg2RoCHWi6-sqL1yRNDqh6F19-syZL9TQndA7m4gFwm*TlsHvmP6YEIjZ09c87gyvlpxwsY*0jhaolaluimYzD3TUuxmlEJZyaqyz1R9*lhz0&new=1',
+# 'https://mp.weixin.qq.com/s?src=11&timestamp=1595380912&ver=2475&signature=oDXd6HO-C*qZ8-UNm14*n2Dn5pnzkxZEQajTZWQnJPItRDkrHnPjvpn7QDKFq3xdpf1vDRPjiMfG1ueOM9gWVnx*UhalXWh1oLlI7HiQKLCGVmLZz*1vxALtm34nzgLb&new=1',
+# 'https://mp.weixin.qq.com/s?src=11&timestamp=1595380912&ver=2475&signature=rlCTjgNvE-Lkk5luRE7UZGI95WxARwp1h4K3CaT8GZdYjPsH3xxdPDCc1Ev585bieYfxRYB9tZFQUVy-tlxkyBVX1PA6Kca2aPSPT*9zgUheBJrPENbc7QYbBC8kJ*8h&new=1'
+#     ]
+    start_urls = [
+        'https://mp.weixin.qq.com/s?src=11&timestamp=1595403610&ver=2475&signature=ctJwiv-2*TQmdkKbTrbSC8Z-h30WsjuW2QyjheDw3qc-A4PCBBD2TSpCK56RjLlBLOpnMOW7IVB*zMyd1pVjRbLYafsbklFIoGPIBAMIoQiEkYHLz6YcV20SJVaL4BPs&new=1'
+    ]
+
+    def getTime(self, response):
+        ct = re.findall('var ct = (.*?);', response.text, re.S)[0]
+        return ct
+
+    def parse(self, response):
+
+        item = GzhItem()
+        item['title'] = response.xpath('//h2[@id="activity-name"]/text()').extract_first().strip()
+        item['url'] = response.url
+        item['content'] = response.xpath('//div[@id="js_content"]').extract_first()
+        item['author'] = response.xpath('//*[@id="js_name"]/text()').extract_first().strip()
+        item['time'] = self.getTime(response)
+        yield item
+        pass
---
+++ TANCMS/spiders/gzh.py
@@ -0,0 +1,162 @@
+import scrapy
+import requests
+import re
+import random
+import json
+from TANCMS.settings import USER_AGENT
+from TANCMS.items import GzhItem
+
+class GzhSpider(scrapy.Spider):
+    name = 'gzh'
+    allowed_domains = ['weixin.sogou.com', 'mp.weixin.qq.com']
+    start_urls = ['https://weixin.sogou.com/weixin?query=核酸检查&_sug_type_=&s_from=input&_sug_=n&type=2&page=1&ie=utf8']
+
+    def getUigsParams(self, response):
+        uigs_para = re.findall('var uigs_para = (.*?);', response.text, re.S)[0]
+        if 'passportUserId ? "1" : "0"' in uigs_para:
+            uigs_para = uigs_para.replace('passportUserId ? "1" : "0"', '0')
+        uigs_para = json.loads(uigs_para)
+        exp_id = re.findall('uigs_para.exp_id = "(.*?)";', response.text, re.S)[0]
+        uigs_para['right'] = 'right0_0'
+        uigs_para['exp_id'] = exp_id[:-1]
+        return uigs_para
+
+
+
+
+    def getSUID(self, cookie_params):
+        url = "https://www.sogou.com/sug/css/m3.min.v.7.css"
+        headers = {
+            "Accept": "text/css,*/*;q=0.1",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+            "Connection": "keep-alive",
+            "Cookie": "SNUID={}; IPLOC={}".format(cookie_params['SNUID'], cookie_params['IPLOC']),
+            "Host": "www.sogou.com",
+            "Referer": "https://weixin.sogou.com/",
+            "User-Agent": USER_AGENT
+        }
+        response2 = requests.get(url, headers=headers)
+        setCookie = response2.headers['Set-Cookie']
+        suid = re.findall('SUID=(.*?);', setCookie, re.S)[0]
+        return suid
+
+    def getJSESSIONID(self, cookie_params, requestUrl, setCookie):
+        url = "https://weixin.sogou.com/websearch/wexinurlenc_sogou_profile.jsp"
+        headers = {
+            "Accept": "*/*",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+            "Connection": "keep-alive",
+            "Cookie": "ABTEST={}; SNUID={}; IPLOC={}; SUID={}".format(cookie_params['ABTEST'], cookie_params['SNUID'],
+                                                                      cookie_params['IPLOC'],
+                                                                      cookie_params['SUID']),
+            "Host": "weixin.sogou.com",
+            "Referer": requestUrl,
+            "User-Agent": USER_AGENT
+        }
+        response3 = requests.get(url, headers=headers)
+        setCookie = response3.headers['Set-Cookie']
+        jsessionId = re.findall('JSESSIONID=(.*?);', setCookie, re.S)[0]
+        return jsessionId
+
+    def getSUV(self, cookie_params, uigs_para):
+        url = "https://pb.sogou.com/pv.gif"
+        headers = {
+            "Accept": "image/webp,*/*",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+            "Connection": "keep-alive",
+            "Cookie": "SNUID={}; IPLOC={}; SUID={}".format(cookie_params['SNUID'], cookie_params['IPLOC'],
+                                                           cookie_params['SUID']),
+            "Host": "pb.sogou.com",
+            "Referer": "https://weixin.sogou.com/",
+            "User-Agent": USER_AGENT
+        }
+        response4 = requests.get(url, headers=headers, params=uigs_para)
+        setCookie = response4.headers['Set-Cookie']
+        suv = re.findall('SUV=(.*?);', setCookie, re.S)[0]
+        return suv
+
+    def getCookisParams(self, response):
+        uigs_para = self.getUigsParams(response)
+
+        setCookieList = response.headers.getlist("Set-Cookie")
+        setCookie = ''
+        for b in setCookieList:
+            setCookie = setCookie + ';' + str(b, encoding="utf-8")
+
+        cookie_params = {
+            "ABTEST": re.findall('ABTEST=(.*?);', setCookie, re.S)[0],
+            "SNUID": re.findall('SNUID=(.*?);', setCookie, re.S)[0],
+            "IPLOC": re.findall('IPLOC=(.*?);', setCookie, re.S)[0],
+            "SUID": re.findall('SUID=(.*?);', setCookie, re.S)[0]
+        }
+
+        cookie_params['SUID'] = self.getSUID(cookie_params)
+
+        cookie_params['JSESSIONID'] = self.getJSESSIONID(cookie_params, response.url, setCookie)
+
+        cookie_params['SUV'] = self.getSUV(cookie_params, uigs_para)
+
+        return cookie_params
+
+    def get_k_h(self, url):
+        b = int(random.random() * 100) + 1
+        a = url.find("url=")
+        url = url + "&k=" + str(b) + "&h=" + url[a + 4 + 21 + b: a + 4 + 21 + b + 1]
+        return url
+
+    def getArticleUrl(self, params, url, request_url):
+        url = self.get_k_h(url)
+
+        headers = {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+            "Connection": "keep-alive",
+            "Cookie": "ABTEST={}; SNUID={}; IPLOC={}; SUID={}; JSESSIONID={}; SUV={}".format(params['ABTEST'],
+                                                                                             params['SNUID'],
+                                                                                             params['IPLOC'],
+                                                                                             params['SUID'],
+                                                                                             params['JSESSIONID'],
+                                                                                             params['SUV']),
+            "Host": "weixin.sogou.com",
+            "Referer": request_url,
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": USER_AGENT
+        }
+        response = requests.get(url, headers=headers)
+
+        fragments = re.findall("url \+= '(.*?)'", response.text, re.S)
+        article_url = ''
+        for i in fragments:
+            article_url += i
+        return article_url
+
+    def parse(self, response):
+
+        params = self.getCookisParams(response)
+
+        ul = response.xpath('/html/body/div[2]/div[1]/div[3]/ul/li')
+        for li in ul:
+            url = 'https://weixin.sogou.com' + li.xpath('./div[2]/h3/a/@href').extract_first()
+            articleUrl = self.getArticleUrl(params, url, response.url)
+            print(articleUrl)
+            yield scrapy.Request(url=articleUrl, callback=self.detailParse)
+
+    def detailParse(self, response):
+        print('---------------111111111111----------------')
+        item = GzhItem()
+        item['title'] = response.xpath('//*[@id="activity-name"]').extract_first()
+        item['url'] = response.url
+        item['author'] = response.xpath('//*[@id="js_name"]').extract_first()
+        item['time'] = response.xpath('//*[@id="publish_time"]').extract_first()
+
+        print(item)
+
+
+
+
+
+
