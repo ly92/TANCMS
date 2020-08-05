3de5e5ba34be4beabd84b66e0e1bdd3fdafad448
ly92 <1364757394@qq.com>
2020-08-05 19:03:49 +0800

    fix

--- TANCMS/spiders/weibo2.py
+++ TANCMS/spiders/weibo2.py
@@ -1,9 +1,11 @@
 import scrapy
 
+
+
 class Weibo2Spider(scrapy.Spider):
     name = 'weibo2'
 
-    base_url = ''
+    base_url = 'https://weibo.com/1730726637/JenFzvV5o?ref=feedsdk&type=comment#_rnd1596622832186'
     word = ''
     page = 1
 
@@ -11,11 +13,11 @@ class Weibo2Spider(scrapy.Spider):
     cankao2 = 'https://weibo.cn/search/mblog?hideSearchFrame=&keyword=核酸检测'
 
     def start_requests(self):
-        print(1)
+        yield scrapy.Request(url=self.base_url, callback=self.parse)
         pass
 
     def parse(self, response):
-        print(2)
+        print(response.text)
         pass
 
     def account_requests(self, name):
