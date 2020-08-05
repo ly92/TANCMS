b3fbb0be99a1065c6acf72f8f536cdba85c6bff3
ly92 <1364757394@qq.com>
2020-08-05 17:22:32 +0800

    tieba

--- TANCMS/spiders/tieba.py
+++ TANCMS/spiders/tieba.py
@@ -1,6 +1,7 @@
 import scrapy
 import re
 from ..libs.timeHelper import strToTimeStamp
+import time
 
 
 class TiebaSpider(scrapy.Spider):
@@ -9,6 +10,8 @@ class TiebaSpider(scrapy.Spider):
     # kw = 贴吧名  qw = 关键字
     tie_url = 'https://tieba.baidu.com/f/search/res?ie=UTF-8&isnew=1&kw={}&qw={}&un=&rn=10&sd=&ed=&sm=1&only_thread=1&pn={}'
     ba_url = 'https://tieba.baidu.com/f/search/fm?ie=UTF-8&qw={}&pn={}'
+    # tid 帖的ID  pid 评论的ID t 13位时间戳
+    comments_url = 'https://tieba.baidu.com/p/comment?tid={}&pid={}&pn=1&t={}'
     word = '核酸检测'
     tieba = '庄河'
     tie_page = 1
@@ -25,7 +28,7 @@ class TiebaSpider(scrapy.Spider):
         url = 'https://tieba.baidu.com/p/6859283852'
         yield scrapy.Request(url=url, callback=self.parse_content)
 
-    #搜索贴吧
+    # 搜索贴吧
     def ba_requests(self):
         print('开始 ba_requests')
 
@@ -35,8 +38,7 @@ class TiebaSpider(scrapy.Spider):
         else:
             return self.ba_page
 
-
-    #解析贴吧列表
+    # 解析贴吧列表
     def parse_ba(self, response):
         print('开始 parse_ba')
         bas = response.xpath('//*[@class="search-forum-list"]/div')
@@ -56,12 +58,12 @@ class TiebaSpider(scrapy.Spider):
                 yield scrapy.Request(url=url, callback=self.parse_ba)
                 break
 
-    #搜索贴吧内容
+    # 搜索贴吧内容
     def tie_requests(self):
         url = self.tie_url.format('', self.word, self.tie_page)
         yield scrapy.Request(url=url, callback=self.parse_tie)
 
-    #解析贴吧内容列表
+    # 解析贴吧内容列表
     def parse_tie(self, response):
         divs = response.xpath('//*[@class="s_post_list"]/div')
         for div in divs:
@@ -73,16 +75,15 @@ class TiebaSpider(scrapy.Spider):
             ba = div.xpath('./a[1]/font/text()').extract_first()
             author = div.xpath('./a[2]/font/text()').extract_first()
             time = div.xpath('./font/text()').extract_first()
-            time = strToTimeStamp(time)
+            time_str = strToTimeStamp(time)
             url = div.xpath('./span/a/@href').extract_first()
             url = 'https://tieba.baidu.com' + re.findall('(.*?)\?', url, re.S)[0]
 
             yield scrapy.Request(url=url, callback=self.parse_content)
 
-
-    #解析贴吧详情
+    # 解析贴吧详情
     def parse_content(self, response):
-        print(response.url)
+
         divs = response.xpath('//*[@id="j_p_postlist"]/div')
         for div in divs:
             content = div.xpath('.//*[@class="d_post_content j_d_post_content "]/text()').extract_first()
@@ -93,25 +94,19 @@ class TiebaSpider(scrapy.Spider):
             bottom_spans = div.xpath('.//*[@class="post-tail-wrap"]/span').getall()
             if len(bottom_spans) == 4:
                 floor = div.xpath('.//*[@class="post-tail-wrap"]/span[3]/text()').extract_first()
-                time = div.xpath('.//*[@class="post-tail-wrap"]/span[4]/text()').extract_first()
+                time_str = div.xpath('.//*[@class="post-tail-wrap"]/span[4]/text()').extract_first()
             if len(bottom_spans) == 3:
                 floor = div.xpath('.//*[@class="post-tail-wrap"]/span[2]/text()').extract_first()
-                time = div.xpath('.//*[@class="post-tail-wrap"]/span[3]/text()').extract_first()
-
-            print({'floor':floor, 'author':author, 'time':time, 'content':content})
-            # print('------------------')
-            # print(floor)
-
-            # print(div.get())
+                time_str = div.xpath('.//*[@class="post-tail-wrap"]/span[3]/text()').extract_first()
+            print('------------------------------')
+            print({'floor':floor, 'author':author, 'time':time_str, 'content':content})
 
             # 评论回复
-            # replay_li = div.xpath('./div[2]/div[2]/div[2]').get()
-            # print(replay_li)
-            # for li in replay_li:
-            #     content2 = li.xpath('./div/span/text()').extract_first()
-            #     author2 = li.xpath('./div/a/text()').extract_first()
-            #     time2 = li.xpath('./div/div/span[3]/text()').extract_first()
-            #     print({'author': author2, 'time': time2, 'content': content2})
+            tid = re.findall('thread_id":(.*?),', div.get(), re.S)[0]
+            pid = re.findall('data-pid="(.*?)"', div.get(), re.S)[0]
+            t = int(time.time() * 1000)
+            comment_url = self.comments_url.format(tid, pid, t)
+            yield scrapy.Request(url=comment_url, callback=self.parse_comments)
 
         page_a = response.xpath('//*[@class="l_pager pager_theme_4 pb_list_pager"]/a')
         for a in page_a:
@@ -121,11 +116,24 @@ class TiebaSpider(scrapy.Spider):
                 yield scrapy.Request(url=url, callback=self.parse_content)
                 break
 
-
-
-
-    def comments_request(self):
-        pass
+    # 解析评论的回复
+    def parse_comments(self, response):
+        url = response.url
+        lis = response.xpath('/html/body/li')
+        for li in lis:
+            author = li.xpath('./div/a/text()').extract_first()
+            content = li.xpath('./div/span/text()').extract_first()
+            time_str = li.xpath('./div/div/span[3]/text()').extract_first()
+            print({'author':author, 'time':time_str, 'content':content})
+        page_a = response.xpath('//*[@class="j_pager l_pager pager_theme_2"]//a')
+        url = re.findall('(.*?)&pn', url, re.S)[0]
+        for a in page_a:
+            title = a.xpath('./text()').extract_first()
+            if title == '下一页':
+                pn = a.xpath('./@href').extract_first().replace('#', '')
+                url = url + '&pn=' + pn + '&t=' + str(int(time.time() * 1000))
+                yield scrapy.Request(url=url, callback=self.parse_comments)
+                break
 
 
 
